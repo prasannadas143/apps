8b3d22d12e310eec539cfe4a33b7d91324a80fe9
Anupam Singh <AnupamSingh123@users.noreply.github.com>
2017-11-16 21:32:29 +0530

    commit for the designe
    commit for the designe

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+/*global $, JQuery, Alert */
+
+$(document).ready(function () {
+	"use strict";
+	// Nice Scroll Code 
+	$("html").niceScroll();
+	$(".panel-body").niceScroll();
+	
+	// Option Page's Table color
+	$("table tr:even").css("background-color", "#f9f9f9");
+	$(".table1 select:even").css("background-color", "#f9f9f9");
+	
+});
