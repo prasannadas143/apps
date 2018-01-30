ce49709fbbae3cf7de50c05250df9416b662a764
Prasanna <prasanna.d@herolocitycorp.com>
2018-01-30 21:25:11 +0530

    shoppingcart clients updated code
    shoppingcart clients updated code

--- shoppingcart/clients/models.py
+++ shoppingcart/clients/models.py
@@ -1,42 +1,71 @@
-# from django.db import models
+from django.db import models
+from django.core import validators
+from phonenumber_field.modelfields import PhoneNumberField
+from shoppingcart.options.countries.models import Countries
+# Create your models here.
 
-# # Create your models here.
+class Clients(models.Model):
+    email = models.EmailField(unique=True,  validators=[validators.validate_email,])
+    password = models.CharField(max_length=50)
+    client_name = models.CharField(max_length=255, blank=True, null=True)
+    phone = PhoneNumberField(unique=True,  blank=False, null=False)
+    website = models.URLField(blank=True, null=True)
+    created = models.DateTimeField(blank=False, null=False)
+    last_login = models.DateTimeField(blank=False, null=False)
+    status =  models.BooleanField(default=False)
+    # order = models.ForeignKey(
+    #         'Order',
+    #         on_delete=models.CASCADE,
+    #        related_name="order_client", related_query_name="order_client" , blank=True,null=True
+    # )    
+    def __str__(self):
+        return self.client_name
 
-# class Clients(models.Model):
-#     email = models.EmailField(unique=True,  validators=[validators.validate_email,])
-#     password = models.TextField(blank=True, null=True)
-#     client_name = models.CharField(max_length=255, blank=True, null=True)
-#     phone = PhoneNumberField(unique=True,  blank=False, null=False)
+    def get_absolute_url(self):
+        return reverse('shoppingcart:client', kwargs={'pk': self.id})
 
-#     url = models.CharField(max_length=255, blank=True, null=True)
-#     created = models.DateTimeField(blank=True, null=True)
-#     last_login = models.DateTimeField(blank=True, null=True)
-#     status = models.CharField(max_length=1)
-# 	# order = models.ForeignKey(
-# 	#         'Order',
-# 	#         on_delete=models.CASCADE,
-# 	#        related_name="order_client", related_query_name="order_client" , blank=True,null=True
-# 	# )    
 
-#     class Meta:
-#         db_table = 'shopping_cart_clients'
+    def get_success_url(self):
+        return reverse('shoppingcart:clients')
 
-# class Addresses(models.Model):
-#     client_id = models.IntegerField(blank=True, null=True)
-#     country_id = models.IntegerField(blank=True, null=True)
-#     state = models.CharField(max_length=255, blank=True, null=True)
-#     city = models.CharField(max_length=255, blank=True, null=True)
-#     client_zip = models.CharField(max_length=255, blank=True, null=True)
-#     address_1 = models.CharField(max_length=255, blank=True, null=True)
-#     address_2 = models.CharField(max_length=255, blank=True, null=True)
-#     name = models.CharField(max_length=255, blank=True, null=True)
-#     is_default_shipping = models.IntegerField(blank=True, null=True)
-#     is_default_billing = models.IntegerField(blank=True, null=True)
-#     client = models.ForeignKey(
-#         'Clients',
-#         on_delete=models.CASCADE,
-#        related_name="client_addresses", related_query_name="client_addresses" , blank=True,null=True
-#     )    
+    class Meta:
+        db_table = 'shoppingcart_clients'
+        ordering = ['-id']
+        unique_together = (('email'),)
 
-#     class Meta:
-#         db_table = 'shopping_cart_addresses'
+
+
+class Addresses(models.Model):
+    country = models.ForeignKey(
+        Countries,
+        on_delete=models.CASCADE,
+        related_name="address_country",  related_query_name="address_country",blank=True,null=True
+        
+        
+    ) 
+    state = models.CharField(max_length=255,blank=False, null=False)
+    city = models.CharField(max_length=255, blank=False, null=False)
+    client_zip = models.CharField(max_length=255,blank=False, null=False)
+    address_1 = models.CharField(max_length=2000,blank=False, null=False)
+    address_2 = models.CharField(max_length=2000, blank=True, null=True)
+    is_default_shipping = models.BooleanField(default=False)
+    is_default_billing =  models.BooleanField(default=False)
+    client = models.ForeignKey(
+        'Clients',
+        on_delete=models.CASCADE,
+       related_name="address_client", related_query_name="address_client" , blank=True,null=True
+    )    
+
+    class Meta:
+        db_table = 'shoppingcart_addresses'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.address_client.client_name
+
+    def get_absolute_url(self):
+        return reverse('shoppingcart:address', kwargs={'pk': self.id})
+
+
+    def get_success_url(self):
+        return reverse('shoppingcart:addresses')
--- shoppingcart/clients/views.py
+++ shoppingcart/clients/views.py
@@ -1,3 +1,87 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt,requires_csrf_token, \
+ensure_csrf_cookie,csrf_protect
+from django.forms.models import model_to_dict
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from copy import deepcopy
+import pdb,os,json
+from shoppingcart.clients.models import Clients,Addresses
+from shoppingcart.clients.form import ClientsForm, AddressesForm
+from shoppingcart.options.countries.models import Countries
+
+@requires_csrf_token
+def addClient(request):
+	pdb.set_trace()
+	if request.method == 'POST':
+		formparams= request.POST.dict()
+		request.POST._mutable = True
+		request.POST.clear()
+		request.POST["email"] = formparams['email']
+		request.POST["password"] = formparams['password']
+		request.POST["phone"] = formparams['phone']
+		request.POST["website"] = formparams['website']
+		request.POST["created"] = formparams['created']
+		request.POST["last_login"] = formparams['last_login']
+        clientform = ClientsForm(request.POST or None )
+      
+        if clientform.is_valid():
+
+            clientobj = clientform.save(commit=False)
+            # clientobj.service = serviceobj
+            # clientobj.employee = employeeobj
+            clientobj.save()
+
+
+	templatename="AddClient.html"
+	clientsform = ClientsForm()
+	addressesform = AddressesForm()
+	listCountry = Countries.objects.values('id', 'CountryName')
+	return render(request,templatename, {'clientsform': clientsform, \
+		'addressesform' : addressesform, "countries" : listCountry } )
+
+
+@requires_csrf_token
+def editClient(request, id=None):
+        templatename="EditClient.html"
+
+        return render(request, templatename ) 
+
+@ensure_csrf_cookie
+def deleteClient(request, id=None):
+
+    aClient=get_object_or_404( Clients,pk=id)
+    aClient.delete()
+    return HttpResponse(status=204)
+@requires_csrf_token
+def Clients(request):
+        templatename="Clients.html"
+
+        return render(request, templatename ) 
+
+@ensure_csrf_cookie
+def ClientList(request):
+    country_info=[]
+
+    # if 'querydata' in request.GET:
+    #     querydata = request.GET['querydata']
+    #     if querydata == "all":
+    #         listCountry = Countries.objects.all().order_by('-id')
+    #     elif querydata == "active":
+    #         listCountry = Countries.objects.filter(status = 1 ).order_by('-id')
+    #     elif querydata == "inactive":
+    #         listCountry = Countries.objects.filter(status = 0 ).order_by('-id')
+    # else:
+    #     listCountry = Countries.objects.all().order_by('-id')
+    # for Country in listCountry:
+    #     data=dict()
+    #     data['id'] = Country.id
+    #     data['CountryName'] = Country.CountryName
+    #     data['Alpha2'] = Country.Alpha2
+    #     data['Alpha3'] = Country.Alpha3
+    #     data['status'] = str(Country.status)
+    #     country_info.append(data)
+    return  HttpResponse(json.dumps({"data" :country_info }), content_type='application/json')   
